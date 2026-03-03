be8785e7c6462f131e4635adae834820371dbbc1
Bisnu Chandra Roy <bisnuchandraroy10@gmail.com>
2026-03-03 10:57:58 -0800

    first commit

---
+++ new.js
@@ -0,0 +1,6 @@
+console.log("Hi this is my first js file after a long time.");
+const age = 25;
+console.log(age, typeof age);
+const roll = "25";
+console.log("sum of roll and age is", age + roll, "because the type of age is", typeof age, 'And the type of roll is', typeof roll);
+const country = "bangladesh";
